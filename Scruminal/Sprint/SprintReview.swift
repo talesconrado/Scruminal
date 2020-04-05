a6432423f138a7aed19b693bbe3d154ca901d981
Tales Conrado <talesconrado@br-ifce-14.local>
2020-04-05 17:01:14 -0300

    First commit

---
+++ Scruminal/Sprint/SprintReview.swift
@@ -0,0 +1,9 @@
+//
+//  SprintReview.swift
+//  Scruminal
+//
+//  Created by Tales Conrado on 04/04/20.
+//  Copyright © 2020 Jéssica & Tales. All rights reserved.
+//
+
+import Foundation
---
+++ Scruminal/main.swift
@@ -0,0 +1,2 @@
+//  Copyright © 2020 Jéssica & Tales. All rights reserved.
+
