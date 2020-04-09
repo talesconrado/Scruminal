73b71887399441fe00d376a0586c1f2cd7a6fdde
Tales Conrado <talesconrado@br-ifce-14.local>
2020-04-09 20:12:29 -0300

    Creates class SprintOnGoing

--- Scruminal/Sprint/SprintOnGoing.swift
+++ Scruminal/Sprint/SprintOnGoing.swift
@@ -1,8 +1,3 @@
-//
-//  SprintOnGoing.swift
-//  Scruminal
-//
-//  Created by Tales Conrado on 04/04/20.
 //  Copyright © 2020 Jéssica & Tales. All rights reserved.
 //
 
@@ -10,6 +5,38 @@
 //checkbox 
 //interation with Kaban
 import Foundation
+
+class SprintOnGoing {
+    
+    //Array of SprintBacklog items
+    //TODO: Change the Int's to SprintBacklog's struct
+    var doingList = [Int]()
+    var doneList = [Int]()
+    var backlogList = [Int]()
+    let sprintBacklog = SprintBacklog()
+    
+    //TODO: change this to use as type the struct from SprintBacklog
+    func addItemToDoingList(listToRemoveFrom: inout [Int], itemIndex:Int){
+        //remove from SprintBacklog and...
+        let itemToAdd = listToRemoveFrom.remove(at: itemIndex)
+        doingList.append(itemToAdd)
+    }
+    
+    //change references to items as int to items as struct from SprintBacklog
+    func addItemToDoneList(listToRemoveFrom: inout [Int], itemIndex: Int){
+        let itemToAdd = listToRemoveFrom.remove(at: itemIndex)
+        doneList.append(itemToAdd)
+    }
+    
+    //this one would be something like "going back" to sprint backlog, not actually just adding
+    func addItemToBacklog(listToRemoveFrom: inout [Int], itemIndex: Int){
+        let itemToAdd = listToRemoveFrom.remove(at: itemIndex)
+        backlogList.append(itemToAdd)
+    }
+    
+    
+    
+}
 
 
 
