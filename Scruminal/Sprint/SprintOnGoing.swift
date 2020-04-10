a57480b9df31cc3cd1f342a176b3341d6a66bd93
Tales Conrado <talesconrado@br-ifce-14.local>
2020-04-09 21:43:29 -0300

    Adds tests to sprintOnGoing

--- Scruminal/Sprint/SprintOnGoing.swift
+++ Scruminal/Sprint/SprintOnGoing.swift
@@ -8,31 +8,29 @@ import Foundation
 
 class SprintOnGoing {
     
-    //Array of SprintBacklog items
-    //TODO: Change the Int's to SprintBacklog's struct
-    var doingList = [Int]()
-    var doneList = [Int]()
-    var backlogList = [Int]()
-    let sprintBacklog = SprintBacklog()
+    typealias ItemSprint = SprintBacklog.ItemSprint
+  
+    var doingList = [ItemSprint]()
+    var doneList = [ItemSprint]()
+    let sprintBacklog:SprintBacklog
     
-    //TODO: change this to use as a type the struct from SprintBacklog
-    func addItemToDoingList(listToRemoveFrom: inout [Int], itemIndex:Int){
-        //remove from SprintBacklog and...
+    init(sprintBacklog:SprintBacklog){
+        self.sprintBacklog = sprintBacklog
+    }
+    
+    func addItemToDoingList(listToRemoveFrom: inout [ItemSprint], itemIndex:Int){
         let itemToAdd = listToRemoveFrom.remove(at: itemIndex)
         doingList.append(itemToAdd)
     }
     
-    //change references to items as int to items as struct from SprintBacklog
-    func addItemToDoneList(listToRemoveFrom: inout [Int], itemIndex: Int){
+    func addItemToDoneList(listToRemoveFrom: inout [ItemSprint], itemIndex: Int){
         let itemToAdd = listToRemoveFrom.remove(at: itemIndex)
         doneList.append(itemToAdd)
     }
     
-    //this one would be something like "going back" to sprint backlog, not actually just adding
-    //TODO: this
-    func addItemToBacklog(listToRemoveFrom: inout [Int], itemIndex: Int){
+    func addItemToBacklog(listToRemoveFrom: inout [ItemSprint], itemIndex: Int){
         let itemToAdd = listToRemoveFrom.remove(at: itemIndex)
-        backlogList.append(itemToAdd)
+        sprintBacklog.listSprint.append(itemToAdd)
     }
     
 }
